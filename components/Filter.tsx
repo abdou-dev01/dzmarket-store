decfef2ef72e9facc4a90afa4013f7721b89b78c
abdou <abdouben-1209@hotmail.com>
2024-11-07 16:38:29 +0100

    fix a lot of errors

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -5,7 +5,6 @@ import { useRouter, useSearchParams } from "next/navigation";
 import queryString from "query-string";
 import Button from "./Button";
 import { cn } from "@/lib/utils";
-import { useEffect } from "react";
 
 interface FilterProps {
   valueKey: string;
@@ -25,17 +24,15 @@ const Filter: React.FC<FilterProps> = ({ valueKey, name, data }) => {
 
     if (current[valueKey] === id) query[valueKey] = null;
 
-    useEffect(() => {
-      const url = queryString.stringifyUrl(
-        {
-          url: window.location.href,
-          query,
-        },
-        { skipNull: true }
-      );
+    const url = queryString.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
 
-      router.push(url);
-    }, [router, window.location.href]);
+    router.push(url);
   };
 
   return (

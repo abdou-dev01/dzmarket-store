0caad25d107c2371d5b092dcce0a09b27b817498
abdou <abdouben-1209@hotmail.com>
2024-11-07 16:25:23 +0100

    fix a lot of errors

--- app/(routes)/cart/components/Summary.tsx
+++ app/(routes)/cart/components/Summary.tsx
@@ -41,7 +41,7 @@ const Summary = () => {
       <h2 className="text-lg font-medium text-gray-800">Order Summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <p className="font-medium text-gray-800">Order total</p>
+          <p className="font-medium text-gray-800 capitalize">order total</p>
           <Currency value={totalPrice} />
         </div>
       </div>
